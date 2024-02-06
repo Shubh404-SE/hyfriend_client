9aedba4b0be4d7593b82d192d6681f11cdaebf86
Shubh404-SE <tanwarshubham29@gmail.com>
2024-02-07 01:59:22 +0530

    context menu hide functionality

--- src/components/common/ContextMenu.jsx
+++ src/components/common/ContextMenu.jsx
@@ -1,9 +1,22 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 function ContextMenu({ options, cordinates, contextMenu, setContextMenu }) {
   const contextRef = useRef(null);
-  const handleClick = (e, callback) => { 
 
+  // to hide context menu on click outside 
+  useEffect(()=>{
+    const handleOutSideClick =(e)=>{
+        if(e.target.id !== "context_opener"){
+          if(contextRef.current && !contextRef.current.contains(e.target))
+          setContextMenu(false);
+        }
+    }
+    document.addEventListener("click", handleOutSideClick)
+  }, [])
+  const handleClick = (e, callback) => { 
+    e.stopPropagation();
+    setContextMenu(false);
+    callback();
   }
 
   return (
@@ -15,7 +28,7 @@ function ContextMenu({ options, cordinates, contextMenu, setContextMenu }) {
       <ul>
         {
           options.map(({name, callback})=>(
-            <li key={name} onClick={()=>handleClick(e, callback)}
+            <li key={name} onClick={(e)=>handleClick(e, callback)}
             className="px-5 py-2 cursor-pointer hover:bg-background-default-hover"
             >
               <span className=" text-white">{name}</span>
