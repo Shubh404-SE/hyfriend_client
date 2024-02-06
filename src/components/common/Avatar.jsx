87173c831d91669db662857d8f879d2ccbf9e0d4
Shubh404-SE <tanwarshubham29@gmail.com>
2024-02-07 01:25:37 +0530

    context manue for image uploading

--- src/components/common/Avatar.jsx
+++ src/components/common/Avatar.jsx
@@ -1,21 +1,29 @@
 import React, { useState } from "react";
 import Image from "next/image";
-import {FaCamera} from "react-icons/fa";
+import { FaCamera } from "react-icons/fa";
+import ContextMenu from "./ContextMenu";
 
 function Avatar({ type, image, setImage }) {
   const [hover, setHover] = useState(false);
   const [isContextMenue, setIsContextMenue] = useState(false);
   const [contextMenueCordinates, setContextMenueCordinates] = useState({
-    x:0,
-    y:0,
+    x: 0,
+    y: 0,
   });
+  const contextMenuOptions = [
+    {name:"Take photo", callback: ()=>{}},
+    {name:"Choose from Library", callback: ()=>{}},
+    {name:"Upload photo", callback: ()=>{}},
+    {name:"Remove photo", callback: ()=>{}}
 
-  const showContextMenue = (e) =>{
+  ]
+
+  const showContextMenue = (e) => {
     e.preventDefault();
     setIsContextMenue(true);
-    setContextMenueCordinates({x:e.pageX, y:pageY});
-  }
-  
+    setContextMenueCordinates({ x: e.pageX, y: e.pageY
+    });
+  };
 
   return (
     <>
@@ -31,19 +39,46 @@ function Avatar({ type, image, setImage }) {
           </div>
         )}
         {type === "xl" && (
-          <div className="relative z-0 cursor-pointer"
-          onMouseEnter={()=>setHover(true)}
-          onMouseLeave={()=>setHover(false)}
-          > 
-          <div className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2 ${hover ? " visible":"hidden"}`}>
-            <FaCamera className=" text-2xl" id="context-opener" />
-            <span className=" text-white">Change profile photo</span>
-          </div>
+          <div
+            className="relative z-0 cursor-pointer"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+            <div
+              className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2 ${
+                hover ? " visible" : "hidden"
+              }`}
+              onClick={(e) => showContextMenue(e)}
+              id="context_opener"
+            >
+              <FaCamera
+                className=" text-2xl"
+                onClick={(e) => showContextMenue(e)}
+                id="context_opener"
+              />
+              <span
+                className=" text-white"
+                onClick={(e) => showContextMenue(e)}
+                id="context_opener"
+              >
+                Change profile photo
+              </span>
+            </div>
             <div className=" h-60 w-60">
               <Image src={image} alt="Avatar" className=" rounded-full" fill />
             </div>
           </div>
         )}
+        <div>
+          {
+            isContextMenue && <ContextMenu 
+            options={contextMenuOptions}
+            cordinates={contextMenueCordinates}
+            contextMenu={isContextMenue}
+            setContextMenu={setIsContextMenue}
+             />
+          }
+        </div>
       </div>
     </>
   );
--- src/components/common/ContextMenu.jsx
+++ src/components/common/ContextMenu.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 
-function ContextMenu() {
-  return <div>ContextMenu</div>;
+function ContextMenu({ options, cordinates, contextMenu, setContextMenu }) {
+  const contextRef = useRef(null);
+  const handleClick = (e, callback) => { 
+
+  }
+
+  return (
+    <div
+      className={`bg-dropdown-background fixed py-2 z-[100] top-[${cordinates.y}] left-[${cordinates.x}] shadow-sm`}
+      ref={contextRef}
+    >
+      <ul>
+        {
+          options.map(({name, callback})=>(
+            <li key={name} onClick={()=>handleClick(e, callback)}
+            className="px-5 py-2 cursor-pointer hover:bg-background-default-hover"
+            >
+              <span className=" text-white">{name}</span>
+            </li>
+          ) )
+        }
+      </ul>
+      ContextMenu
+    </div>
+  );
 }
 
 export default ContextMenu;
