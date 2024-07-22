f59793cb5fb40fe228b63515efb9aeeccec2b77e
Shubh404-SE <tanwarshubham29@gmail.com>
2024-07-23 00:51:11 +0530

    app linked with zego cloud for calling service. need to fix few bugs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env:{
+    NEXT_PUNLIC_ZEGO_APP_ID: 530545002,
+    NEXT_PUNLIC_ZEGO_SERVER_ID:"bf3de27e6a0e37c0ac3468e25a5450f7"
+  },
   images: {
     domains: ['lh3.googleusercontent.com', "localhost"],
   },
